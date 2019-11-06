fb9aa6889f21c28a02607534bfcd73de414dbb9e
TKSun21 <kietnguyen2016@gmail.com>
2019-11-06 15:56:41 -0500

    Did as much as we can, was struggling

--- ArrayListPractice/src/com/revature/list/MyArrayList.java
+++ ArrayListPractice/src/com/revature/list/MyArrayList.java
@@ -2,29 +2,37 @@ package com.revature.list;
 
 public class MyArrayList {
 
+	private int[] array = new int[5];
+	private int size;
+	
 	public void add(int val) {
 		// TODO Auto-generated method stub
-		
+		array[size] = val;
+		size++;	
 	}
 
 	public void set(int index, int val) {
 		// TODO Auto-generated method stub
-		
+		mylist.set(2, 10);
+		mylist.set(4, 20);
 	}
 
 	public void remove(int index) {
 		// TODO Auto-generated method stub
-		
+		array[size] = index;
+		size--;
 	}
 
 	public int get(int index) {
 		// TODO Auto-generated method stub
-		return 0;
+		mylist.get(index);
+		return index;
 	}
 
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
+		System.out.println(mylist);
 		return null;
 	}
 }
